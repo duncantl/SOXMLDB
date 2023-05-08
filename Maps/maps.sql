b4d556b473b3c2330e2b123b20f52ab86e380751
Duncan Temple Lang <duncan@r-project.org>
2023-05-08 06:23:59 -0700

    remove PostHistoryTypeMap and keep PostHistoryTypeId. Probably should swap the names

--- Maps/maps.sql
+++ Maps/maps.sql
@@ -9,10 +9,17 @@ CREATE TABLE BadgeClassMap (
   value TEXT
 );
 
-CREATE TABLE PostHistoryTypeMap (
-  id INTEGER,
-  value TEXT
+#- CREATE TABLE PostHistoryTypeMap (
+#-  id INTEGER,
+#-  value TEXT
+#- );
+
+CREATE TABLE PostHistoryTypeId(
+  Id INTEGER PRIMARY KEY,
+  Label TEXT,
+  Description TEXT
 );
+
 
 CREATE TABLE CloseReasonMap (
   id INTEGER,
@@ -34,7 +41,10 @@ CREATE TABLE VoteTypeMap (
 .mode csv
 .import PostTypeIdMap.csv   PostTypeIdMap
 .import BadgeClassMap.csv  BadgeClassMap
-.import PostHistoryTypeMap.csv  PostHistoryTypeMap
 .import CloseReason.csv  CloseReasonMap
 .import LinkTypeMap.csv    LinkTypeMap
 .import VoteType.csv  VoteTypeMap
+.import PostHistoryTypeId.csv PostHistoryTypeId
+
+
+# .import PostHistoryTypeMap.csv  PostHistoryTypeMap
