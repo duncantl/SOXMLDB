5ebad5bbd9ffe8ed06897f86ac1661114016236f
Duncan Temple Lang <duncan@r-project.org>
2023-05-08 06:58:12 -0700

    check that tags in Posts and TagPosts

--- RCheckCode/checkTags.R
+++ RCheckCode/checkTags.R
@@ -1,3 +1,22 @@
+# d is the list of all the tables from reading the RDS. See ../mkRDS.R
+#
+# Check all the elements in all of the Tag values in Posts are in the TagPosts table
+#
+pt = gsub("^<|>$", "", unlist(strsplit(d$Posts$Tags, "><", fixed = TRUE)))
+stopifnot(all(pt %in% d$TagPosts$Tag))
+
+# Now check that combining the tag values in TagPosts by question Id matches
+# the string in Posts$Tag for that question.
+# Ignore the non-questions in the Posts table.
+tagStr = tapply(d$TagPosts$Tag, d$TagPosts$Id, function(x) paste( "<", x, ">", sep = "", collapse = ""))
+isQ = (d$Posts$PostTypeId == 1)
+stopifnot(all(as.character(d$Posts$Id[isQ]) %in% names(tagStr)))
+stopifnot(d$Posts$Tag[isQ] == tagStr[as.character(d$Posts$Id[isQ])])
+
+
+
+#
+if(FALSE) {
 a = read.table("bar1", sep = "\t", header = TRUE, stringsAsFactors = FALSE, comment.char = '')
 b = read.table("bar", sep = "\t", header = TRUE, stringsAsFactors = FALSE, comment.char = '')
 a.none = a$Id[ a$Tags == ""]
@@ -21,5 +40,5 @@ j = names(a.tmp)[!i]
 cbind(a.tmp[j], tmp[j])
 
 z = setdiff(names(tmp), names(a.tmp))
-
+}
 
